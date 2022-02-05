499cd159947232e8ea8b3857b2bcc1849b8ee45a
MattheusAlves <matheus_alves0101@outlook.com>
2022-02-04 21:04:34 -0300

    test: tests for input components
    added unit tests for input component
    
    Signed-off-by: MattheusAlves <matheus_alves0101@outlook.com>

---
+++ Aula 4/ui-testing/src/components/TextInput/Input.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Input from "./Index";
+
+describe("TextInput", () => {
+  const onChange = jest.fn();
+  beforeEach(() => {
+    render(<Input label="fizz" onChange={onChange} name="jest" />);
+  });
+
+  it("should render input with label", () => {
+    expect(screen.getByText("fizz")).toBeInTheDocument();
+    expect(screen.getByTestId("input_jest")).toBeVisible();
+  });
+
+  it("should call onChange when input changes", () => {
+   
+    fireEvent.change(screen.getByTestId("input_jest"), {
+      target: {
+        value: "foo",
+      },
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+  });
+});
